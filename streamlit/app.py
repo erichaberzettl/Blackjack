57d7a1f6a6058a8e51119ea918bdbdff6db9114d
eric <eric.hab19@gmail.com>
2025-11-23 22:38:37 +0100

    first version of simulation configurator using streamlit

---
+++ streamlit/app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+
+
+
+st.title("Blackjack Simulator")
+
+st.header("Simlution Configurator")
+
+
+
+
+with st.container():
+
+    c1, c2 = st.columns(2)
+    with c1:
+        with st.container(border=True):
+            st.markdown("Number of players")
+            player_no = st.slider(label=" ", min_value=1, max_value=6, value=1, key="player_no")
+
+    with c2:
+        with st.container(border=True):
+            st.markdown("Number of rounds")
+            rounds = st.slider(label=" ", min_value=1, max_value=10000, value=100, key="rounds")
+
+    if "players" not in st.session_state:
+        st.session_state.players = {}
+    
+    for i in range(player_no):
+        
+        if i not in st.session_state.players:
+            st.session_state.players[i] = {"hands": 1, "strat": "Basic Strategy", "bet_size": 1}
+
+        with st.container(border=True):
+            col1, col2, col3, col4 = st.columns([2,3,3,3], gap="small")
+            col1.text(f"Player {i+1}")
+            hands = col2.selectbox("Hands per round", [1,2,3,4,5], key=f"hands_player_{i}", index=st.session_state.players[i]["hands"] -1)
+            strats = ["Basic Strategy", "No Bust Strategy", "Custom"]
+            strat = col3.selectbox("Strategy", strats, key=f"strat_player_{i}", index=strats.index(st.session_state.players[i]["strat"]))
+            bet_size = col4.number_input("Bet per hand", min_value=1, max_value=100, key=f"bet_size_{i}", value= st.session_state.players[i]["bet_size"])
+            st.session_state.players[i]["hands"] = hands
+            st.session_state.players[i]["strat"] = strat
+            st.session_state.players[i]["bet_size"] = bet_size
+
+            if st.session_state.players[i]["strat"] == "Custom":
+                st.popover("Create your own strategy")
+
+
+col1, col2 = st.columns(2)
+with col1:
+
+    with st.container(border=True):
+        st.markdown("Dealer: Hit/Stand on soft 17")
+    
+        st.segmented_control(" ", ["H17", "S17"], selection_mode="single", key="dealer_mode", width="stretch")
+
+with col2:
+    with st.container(border=True):
+        st.markdown("Shoe size")
+        st.segmented_control("Number of decks used", [1, 2, 4, 6, 8], selection_mode="single", key="shoe_size", width="stretch")
+
+
+with st.container(border=True):
+        st.markdown("Shuffle mode")
+        shuffle_mode = st.segmented_control(" ", ["Cut-card", "Continuous"], 
+                             selection_mode="single", key="shuffledf_mode", width="stretch")
+        auto_shuffle = True if shuffle_mode == "Continuous" else False
+        st.markdown("Shoe penetration level")
+        penetration_level = st.number_input(format="%0.1f", label="Shoe penetration until shuffle as decimal", min_value=0.1, 
+                                            max_value=1.0, value=0.8, step=0.1, disabled=auto_shuffle, key="penetration")
+
